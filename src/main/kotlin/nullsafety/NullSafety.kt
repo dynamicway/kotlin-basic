a69d331e174f21e807266d57ea4651b0418749de
bee <rkddbtjd123@gmail.com>
2021-09-26 22:18:27 +0900

    feat: add nullsafety package

--- src/main/kotlin/nullsafety/NullSafety.kt
+++ src/main/kotlin/nullsafety/NullSafety.kt
@@ -1,4 +1,5 @@
 package nullsafety
 
 class NullSafety {
+
 }
--- src/test/kotlin/nullsafety/NullSafetyTest.kt
+++ src/test/kotlin/nullsafety/NullSafetyTest.kt
@@ -1,5 +1,23 @@
 package nullsafety
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-internal class NullSafetyTest
+internal class NullSafetyTest {
+
+    @Test
+    fun smartCast() {
+        // given
+        val name: String? = "Not Null"
+        val realName: String
+
+        // when
+
+        // then
+        if (name != null) {
+            realName = name
+            assertNotNull(realName)
+        }
+
+    }
+}
