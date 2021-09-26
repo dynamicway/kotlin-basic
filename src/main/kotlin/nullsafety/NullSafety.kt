e8be487d020a76bd0674ad0ce71dc0301aafb755
bee <rkddbtjd123@gmail.com>
2021-09-26 23:15:20 +0900

    feat: Add NullSafety method

--- src/main/kotlin/nullsafety/NullSafety.kt
+++ src/main/kotlin/nullsafety/NullSafety.kt
@@ -2,4 +2,15 @@ package nullsafety
 
 class NullSafety {
 
+    fun smartCast(name: String?): String {
+        return when (name) {
+            null -> ""
+            else -> name
+        }
+    }
+
+    fun elvisOperator(name: String?): String {
+        return name ?: "default"
+    }
+
 }
