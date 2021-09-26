3701858877229826394c4ebc19c7a95364fa3c7b
bee <rkddbtjd123@gmail.com>
2021-09-26 22:28:57 +0900

    test: Add NullSafetyTest::smartCast_not_working case

--- src/test/kotlin/nullsafety/NullSafetyTest.kt
+++ src/test/kotlin/nullsafety/NullSafetyTest.kt
@@ -11,13 +11,31 @@ internal class NullSafetyTest {
         val name: String? = "Not Null"
         val realName: String
 
-        // when
-
         // then
         if (name != null) {
             realName = name
             assertNotNull(realName)
         }
 
+    }
+
+    @Test
+    fun smartCast_not_working() {
+        // given
+        class Person(var name: String?)
+        var person = Person(null)
+        val length: Int
+
+        // then
+        if (person.name != null) {
+//            length = person.name.length ** Cannot Call **
+            length = person.name!!.length // not null assertion operator
+            assertNotNull(length)
+        }
+
+        person.name?.let {
+            val safeCallLength = it.length
+            assertNotNull(safeCallLength)
+        }
     }
 }
